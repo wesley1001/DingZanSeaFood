bb7259d816f8fe20d88a8d06099f16918a2ca694
summer-breeze <chengjie.shen@hotmail.com>
2016-01-15 22:36:16 +0800

    商品详情页面增加完毕

---
+++ BreezeShop.Web/Controllers/AjaxController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using BreezeShop.Core;
+using BreezeShop.Core.DataProvider;
+using Yun.Trade.Request;
+
+namespace BreezeShop.Web.Controllers
+{
+    public class AjaxController : MemberAuthController
+    {
+        public ActionResult DeleteCart(string cartId)
+        {
+            var r =
+                YunClient.Instance.Execute(new RemoveCartRequest {CartIds = cartId},
+                    Member.Token);
+
+            return Json(new {error = r.ErrMsg, cartid = r.Result}, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult UpdateCard(int cartId, int quantity)
+        {
+            var r = YunClient.Instance.Execute(new UpdateCartRequest
+            {
+                CartId = cartId,
+                Quantity = quantity
+            }, Member.Token);
+
+            return Json(new { error = r.ErrMsg, result = r.Result }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AddCart(int itemId, int quantity, string delivery, int skuId = 0)
+        {
+            var r = YunClient.Instance.Execute(new AddToCartRequest
+            {
+                ItemId = itemId,
+                Quantity = quantity,
+                SkuId = skuId,
+                Delivery = delivery
+            }, Member.Token);
+
+            return r.Result > 0
+                ? Json(new {cartid = r.Result}, JsonRequestBehavior.AllowGet)
+                : Json(new {error = r.ErrMsg, cartid = r.Result}, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
--- BreezeShop.Web/Controllers/HomeController.cs
+++ BreezeShop.Web/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using BreezeShop.Core;
 using BreezeShop.Core.DataProvider;
 using Yun.Item.Request;
+using Yun.Logistics.Request;
 
 namespace BreezeShop.Web.Controllers
 {
@@ -57,9 +58,43 @@ namespace BreezeShop.Web.Controllers
             return PartialView(req.Items);
         }
 
-        public ActionResult GoodsDetail()
+        public ActionResult Buy()
         {
             return View();
         }
+
+        public ActionResult GoodsDetail(int id = 0)
+        {
+            if (id <= 0)
+            {
+                return Content("商品不存在");
+            }
+
+            var item = YunClient.Instance.Execute(new GetItemRequest {Id = id}).Item;
+            if (item != null && !item.IsDelete && item.ItemState == 1)
+            {
+                if (item.ItemSkus != null && item.ItemSkus.Any())
+                {
+                    //生成SKU的JSON数据
+                    var skuData = item.ItemSkus.Where(e => e.Stock > 0)
+                        .Aggregate("{",
+                            (current, sku) =>
+                                current +
+                                string.Format("\"{0}\": {{ price:{1},  count:{2}, skuid:{3} }},",
+                                    string.Join(";", sku.SpecIds), sku.Price, sku.Stock, sku.Id));
+                    skuData = skuData.Trim(',');
+                    skuData += "}";
+                    ViewData["SkuData"] = skuData;
+                }
+
+                ViewData["ExpressTemplate"] =
+                    YunClient.Instance.Execute(new GetExpressTemplateRequest {Id = (int) item.FreightTemplateId}).Result;
+
+                return View(item);
+            }
+
+            return Content("商品不存在");
+        }
+
     }
 }
